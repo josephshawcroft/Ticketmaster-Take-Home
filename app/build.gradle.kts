c88295446feb36fad38a75bb3d61ad94b710c429
Joe Shawcroft <joeymoto@hotmail.co.uk>
2023-09-15 14:29:10 +0100

    Added Dagger Hilt, API key and updated various dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,16 +3,20 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
+
 android {
     namespace = "com.shawcroftstudios.ticketmastertakehome"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.shawcroftstudios.ticketmastertakehome"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
+        val apiKey: String by properties
+        buildConfigField("String", "API_KEY", "\"${apiKey}\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -37,6 +41,7 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
+        buildConfig = true
         compose = true
     }
     composeOptions {
@@ -51,14 +56,24 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    // AndroidX
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // DI
+    implementation("com.google.dagger:hilt-android:2.41")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    // Network
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Test
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
