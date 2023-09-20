78a88fa1c551e90a0d8bc9787ff7fe50a5bfa026
Joe Shawcroft <joeymoto@hotmail.co.uk>
2023-09-20 14:22:26 +0100

    Fixed bug where search bar text would clear upon orientation change

--- app/src/main/java/com/shawcroftstudios/ticketmastertakehome/ui/composable/SearchBar.kt
+++ app/src/main/java/com/shawcroftstudios/ticketmastertakehome/ui/composable/SearchBar.kt
@@ -19,7 +19,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -39,7 +39,7 @@ import com.shawcroftstudios.ticketmastertakehome.R
 @Composable
 fun SearchBar(modifier: Modifier = Modifier, onQueryChange: (String) -> Unit) {
 
-    var query by remember { mutableStateOf("") }
+    var query by rememberSaveable { mutableStateOf("") }
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
 
